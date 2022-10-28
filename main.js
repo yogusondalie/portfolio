97ff876d4a33a7e91a96910e9507c9d5f729c957
yogusdalie <canghl24@gmail.com>
2022-10-28 22:25:33 +1000

    JS-navbar

---
+++ main.js
@@ -0,0 +1,13 @@
+'user strict'
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+    /**checking navbar Height */
+    // console.log(window.scrollY);
+    // console.log(`navbarHeight: ${navbarHeight}`);
+    if (window.scrollY > navbarHeight) {
+        navbar.classList.add('navbar--dark');
+    } else {
+        navbar.classList.remove('navbar--dark');
+    }
+});
