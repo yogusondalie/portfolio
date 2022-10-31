f7f50e74d1d8417653a859b96b1c48769454d49e
yogusdalie <canghl24@gmail.com>
2022-10-31 22:34:49 +1000

    JS-home opacity, navbar changed color

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 'user strict'
+// Decorated navbar
 const navbar = document.querySelector('#navbar');
 const navbarHeight = navbar.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
@@ -11,3 +12,43 @@ document.addEventListener('scroll', () => {
         navbar.classList.remove('navbar--dark');
     }
 });
+
+// link with each page to scroll to view
+const navbarMenu = document.querySelector('.navbar__menu');
+navbarMenu.addEventListener('click', (event) => {
+    const target = event.target;
+    const link = target.dataset.link;
+    if (link == null) {
+        return;
+    }
+    scrollIntoView(link);
+});
+
+// contact btn links to contact page
+const contactBtn = document.querySelector('.home__contact');
+contactBtn.addEventListener('click', (e) => {
+    scrollIntoView('#contact');
+});
+
+function scrollIntoView(slector) {
+    const scrollTo = document.querySelector(slector);
+    scrollTo.scrollIntoView({
+        behavior: 'smooth'
+    });
+}
+
+//change home opacity when user scrolls down the page
+const home = document.querySelector('.home__container');
+const homeHeight = home.getBoundingClientRect().height;
+console.log(window.scrollY);
+console.log(`homeHeight: ${homeHeight}`);
+document.addEventListener('scroll', () => {
+    home.style.opacity = 1 - window.scrollY / homeHeight
+});
+// const homeChange = document.addEventListener('scroll',
+//     function (e) {
+//         if (window.scrollY > 500) {
+//             const home = document.getElementById("#home");
+//             home.classList.add("reduced-opacity");
+//         }
+//     });
